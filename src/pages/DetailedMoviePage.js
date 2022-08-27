48c6c173aff2519953c4bf01d55caf982085b218
Edo Adam Jarror <77976169+edojarror@users.noreply.github.com>
2022-08-27 15:05:33 +0700

    adopt redux at DetailedMoviePage, delete data folder so app will be depent on redux store state

--- src/pages/DetailedMoviePage.js
+++ src/pages/DetailedMoviePage.js
@@ -1,26 +1,31 @@
 import { useParams } from 'react-router-dom';
-import { getMovie } from '../data/testDataMovies';
+import { connect } from 'react-redux';
 import { Box, Grid,Card, CardMedia, Typography } from '@mui/material';
 import DetailedText from '../components/DetailedText';
-export default function DetailedMoviePage () {
+
+const mapStateToProps = state => ({moviesData : state.moviesData})
+
+const DetailedMoviePage = ({ moviesData }) => {
     let params = useParams();
-    let currentDataGame = getMovie(params.movieId)
+    const findingDetailedMovie = (targetedId, targetedData) => targetedData.find(movie => movie.id === Number(targetedId))
+    let movieData = findingDetailedMovie(params.movieId, moviesData)
+    
     return (
         <Box boxSizing="border-box">
 
             <Box margin={4}>
-                <Typography variant='h3' textAlign="center">{currentDataGame.title}</Typography>
+                <Typography variant='h3' textAlign="center">{movieData.title}</Typography>
             </Box>
 
             <Grid container display="flex" flexDirection="row">
                 <Grid item  lg={6} >
                         <Box display="flex" flexDirection="column"  padding={2}>
-                            <DetailedText firstText="Release Date : " secondText={currentDataGame['release-date']} marginBtm={2}/>
-                            <DetailedText firstText="Duration : " secondText={currentDataGame.duration} marginBtm={2} />
-                            <DetailedText firstText="Director : " secondText={currentDataGame.director} marginBtm={2} />
-                            <DetailedText firstText="Writers : " secondText={currentDataGame.writers} marginBtm={2} />
-                            <DetailedText firstText="Stars : " secondText={currentDataGame.stars} marginBtm={2} />
-                            <DetailedText firstText="Genre : " secondText={currentDataGame.genre} marginBtm={2} />
+                            <DetailedText firstText="Release Date : " secondText={movieData['release-date']} marginBtm={2}/>
+                            <DetailedText firstText="Duration : " secondText={movieData.duration} marginBtm={2} />
+                            <DetailedText firstText="Director : " secondText={movieData.director} marginBtm={2} />
+                            <DetailedText firstText="Writers : " secondText={movieData.writers} marginBtm={2} />
+                            <DetailedText firstText="Stars : " secondText={movieData.stars} marginBtm={2} />
+                            <DetailedText firstText="Genre : " secondText={movieData.genre} marginBtm={2} />
                         </Box>
                 </Grid>
                 <Grid item  lg={6} sx={{margin: "0 auto"}}>
@@ -28,9 +33,9 @@ export default function DetailedMoviePage () {
                         <Card>
                             <CardMedia 
                                 component="img" 
-                                image={currentDataGame.image} 
+                                image={movieData.image} 
                                 height="400"  
-                                alt={currentDataGame.title}
+                                alt={movieData.title}
                              />
                         </Card>
                     </Box>
@@ -38,8 +43,10 @@ export default function DetailedMoviePage () {
             </Grid>
 
             <Box marginTop={{lg: "12vh", xl: 16, md: 0, sm: 0, xs: 2}}>
-                <DetailedText firstText="Description : " secondText={currentDataGame.description} marginBtm={2} />
+                <DetailedText firstText="Description : " secondText={movieData.description} marginBtm={2} />
             </Box>
         </Box>
     )
 }
+
+export default connect(mapStateToProps) (DetailedMoviePage)
